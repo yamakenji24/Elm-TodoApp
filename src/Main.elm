af89538a6e1d519608de6952ac7e6443ecc64b48
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2021-06-27 02:40:27 +0900

    feat/todo add feature

--- src/Main.elm
+++ src/Main.elm
@@ -1,56 +1,114 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Html exposing (..)
+import Html.Events exposing (onClick, onSubmit, onInput)
+import Html.Attributes exposing (class, disabled, placeholder, type_, value)
 
 
 ---- MODEL ----
 
-
 type alias Model =
-    {}
+  {
+    todos : List Todo,
+    newTitle : String,
+    newDescription : String
+  }
+type alias Todo =
+  {
+    title : String,
+    description : String
+  }
 
-
-init : ( Model, Cmd Msg )
+init : Model
 init =
-    ( {}, Cmd.none )
+  { 
+    todos = [],
+    newTitle = "",
+    newDescription = ""
+  }
 
 
 
 ---- UPDATE ----
 
-
 type Msg
-    = NoOp
+  = AddTodo
+  | UpdateTodoTitle String
+  | UpdateTodoDescription String
 
-
-update : Msg -> Model -> ( Model, Cmd Msg )
+update : Msg -> Model -> Model
 update msg model =
-    ( model, Cmd.none )
+  case msg of
+    AddTodo -> addNewTodo model 
+    UpdateTodoTitle title -> 
+      { 
+        model | newTitle = title
+      }
+    UpdateTodoDescription description->
+      {
+        model | newDescription = description
+      }
+
+addNewTodo : Model -> Model
+addNewTodo model = 
+  let
+    title = String.trim model.newTitle
+    description = String.trim model.newDescription
+  in
+  case title of 
+    "" -> model
+    _ -> 
+      {
+        model | todos = model.todos ++ [{title = title, description = description}],
+        newTitle = "",
+        newDescription = ""
+      }
 
 
 
 ---- VIEW ----
 
-
 view : Model -> Html Msg
 view model =
-    div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
-        ]
+  div [] [ 
+    viewTable model.todos,
+    form [ class "new todo", onSubmit AddTodo] [
+      input [
+        type_ "text", placeholder "Add new Todo title", value model.newTitle, onInput UpdateTodoTitle
+      ] [],
+      input [
+        type_ "text", placeholder "Add new Todo description", value model.newDescription, onInput UpdateTodoDescription
+      ] [],
+      button [ disabled (String.isEmpty model.newTitle)] [text "Add"]
+    ]
+  ]
 
+viewTable : List Todo -> Html Msg
+viewTable todos = 
+  table [] [
+    thead [] [
+      th [] [text "Title"],
+      th [] [text "Description"]
+    ],
+    tbody [] (List.map viewTr todos)
+  ]
+
+viewTr : Todo -> Html Msg
+viewTr todo = 
+  tr [] [
+    td [] [text todo.title],
+    td [] [text todo.description]
+  ]
 
 
 ---- PROGRAM ----
 
-
 main : Program () Model Msg
 main =
-    Browser.element
-        { view = view
-        , init = \_ -> init
-        , update = update
-        , subscriptions = always Sub.none
-        }
+  Browser.sandbox
+    { 
+      view = view, 
+      init =  init, 
+      update = update
+    }
